21608cabadf1aa94ebabb9fc64621e49f3615ddb
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-07-21 10:34:24 +0700

    Add files via upload

---
+++ HangTu.java
@@ -0,0 +1,25 @@
+package DaThuc;
+
+class HangTu {
+    int n; //so mu
+    double hs; //hs
+
+    HangTu(int a, double b){
+        n = a;
+        hs = b;
+    }
+
+    //get,set:
+    void setSoMu(int a){
+        n = a;
+    }
+    void setHeSo(double a){
+        hs = a;
+    }
+    int getSoMu(){
+        return n;
+    }
+    double getHeSo() {
+        return hs;
+    }
+}
---
+++ TestDaThuc.java
@@ -0,0 +1,28 @@
+package DaThuc;
+
+public class TestDaThuc {
+
+    public static void main(String[] args) {
+
+        HangTu a = new HangTu(2,5);
+        HangTu b = new HangTu(1,1);
+
+        DaThuc d1 = new DaThuc(a);
+        DaThuc d2= new DaThuc( new HangTu(1,1));
+
+        d1.dt.add(a); d1.dt.add(a); d1.dt.add(a); d1.dt.add(a);
+        d2.dt.add(new HangTu(6,2));
+
+        d1.inDaThuc();
+        d2.inDaThuc();
+
+
+        System.out.print("\nda thuc d1 cong hang tu (1,1) : ");
+        d1.congHangTu(b);
+        d1.inDaThuc();
+
+        System.out.print("\nTong hai da thuc la: ");
+        DaThuc d3 = d1.addDaThuc(d2);
+        d3.inDaThuc();
+    }
+}
