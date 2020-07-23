d00f6d7c9305db93013c37d429fd04aec6a9dcec
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-07-23 12:01:46 +0700

    Update DaThuc.java

--- DaThuc.java
+++ DaThuc.java
@@ -1,4 +1,4 @@
-package DaThuc;
+package dathuc;
 
 import javax.crypto.NullCipher;
 import java.util.LinkedList;
@@ -6,11 +6,12 @@ import java.util.LinkedList;
 import static java.lang.Math.pow;
 
 class DaThuc {
+    
     // mot da thuc la danh sach cac hang tu:
     LinkedList<HangTu> dt = new LinkedList<HangTu>();
 
 
-    DaThuc(){}
+    DaThuc() {}
 
     DaThuc( HangTu a) {
         dt.add(a);
@@ -18,14 +19,14 @@ class DaThuc {
 
 
     //Cong hang tu vao da thuc:
-    void congHangTu( HangTu a){
+    void congHangTu( HangTu a) {
         dt.add(a);
         xuliDaThuc();
     }
 
     //Nhan 1 hang tu vao da thuc:
-    void nhanHangTu( HangTu a){
-        for(int i=0; i<dt.size(); i++){
+    void nhanHangTu( HangTu a) {
+        for(int i=0; i<dt.size(); i++) {
             HangTu b = new HangTu( dt.get(i).getSoMu()*a.getSoMu(), dt.get(i).getHeSo()*a.getHeSo());
             dt.set(i,b);
         }
@@ -33,8 +34,8 @@ class DaThuc {
 
     //xu ly da thuc:
     void xuliDaThuc() {
-        for(int i=0; i<dt.size()-1; i++){
-            for(int j=i+1; j<dt.size(); j++){
+        for(int i=0; i<dt.size()-1; i++) {
+            for(int j=i+1; j<dt.size(); j++) {
                 if(dt.get(i).getSoMu()==dt.get(j).getSoMu()) {
                     HangTu a = new HangTu(dt.get(i).getSoMu(), dt.get(i).getHeSo() + dt.get(j).getHeSo());
                     dt.set(i, a);
@@ -42,7 +43,8 @@ class DaThuc {
                 }
             }
         }
-        for(int i=0; i<dt.size(); i++){
+        
+        for(int i=0; i<dt.size(); i++) {
             if(dt.get(i).getSoMu()==0 && dt.get(i).getHeSo()==0) {
                 dt.remove(i);
                 i--;
@@ -51,7 +53,7 @@ class DaThuc {
     }
 
     //Cong 2 da thuc:
-    DaThuc addDaThuc(DaThuc d2){
+    DaThuc addDaThuc(DaThuc d2) {
         DaThuc d3 = new DaThuc();
         d3.dt.addAll(dt);
         d3.dt.addAll(d2.dt);
@@ -60,12 +62,12 @@ class DaThuc {
     }
 
     //in:
-    void inDaThuc(){
+    void inDaThuc() {
         xuliDaThuc();
         //sap xep:
-        for(int i=0; i<dt.size()-1; i++){
-            for(int j=i+1; j<dt.size(); j++){
-                if(dt.get(i).getSoMu()>dt.get(j).getSoMu()){
+        for(int i=0; i<dt.size()-1; i++) {
+            for(int j=i+1; j<dt.size(); j++) {
+                if(dt.get(i).getSoMu()>dt.get(j).getSoMu()) {
                     HangTu a = new HangTu(dt.get(i).getSoMu(),dt.get(i).getHeSo());
                     dt.set(i,dt.get(j));
                     dt.set(j,a);
@@ -74,8 +76,8 @@ class DaThuc {
         }
         //In:
         System.out.print("\n"+dt.get(0).getHeSo()+"*x^"+dt.get(0).getSoMu());
-        for(int i=1; i<dt.size(); i++){
-            if(dt.get(i).getHeSo()>=0){
+        for(int i=1; i<dt.size(); i++) {
+            if(dt.get(i).getHeSo()>=0) {
                 System.out.print(" + "+dt.get(i).getHeSo()+"*x^"+dt.get(i).getSoMu());
             }
             else{
@@ -85,9 +87,9 @@ class DaThuc {
     }
 
     //tinh gia tri:
-    double tinhDaThuc(double x){
+    double tinhDaThuc(double x) {
         double gt = 0;
-        for(int i=0; i<dt.size(); i++){
+        for(int i=0; i<dt.size(); i++) {
             gt += dt.get(i).getHeSo()* pow(x,dt.get(i).getSoMu());
         }
         return gt;
