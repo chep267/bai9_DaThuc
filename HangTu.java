4d16bd24e24a14f9b0e734cc973efc6994084d36
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-07-23 12:02:27 +0700

    Update HangTu.java

--- HangTu.java
+++ HangTu.java
@@ -1,24 +1,29 @@
-package DaThuc;
+package dathuc;
 
 class HangTu {
+    
     int n; //so mu
     double hs; //hs
 
-    HangTu(int a, double b){
+    HangTu(int a, double b) {
         n = a;
         hs = b;
     }
 
     //get,set:
-    void setSoMu(int a){
+    
+    void setSoMu(int a) {
         n = a;
     }
-    void setHeSo(double a){
+    
+    void setHeSo(double a) {
         hs = a;
     }
+    
     int getSoMu(){
         return n;
     }
+    
     double getHeSo() {
         return hs;
     }
